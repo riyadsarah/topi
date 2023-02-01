740693187066261148654dfda50a024b2222835a
riyadsarah <reysarah901@gmail.com>
2023-02-01 09:12:58 +0700

    jkdsahfj

--- js/main.js
+++ js/main.js
@@ -110,5 +110,33 @@ window.addEventListener("load", function(){
 
         // Select the button
 
+     // Set the date we're counting down to
+var countDownDate = new Date("Feb 3, 2023 23:59:59").getTime();
 
-        
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Display the result in the element with id="timer"
+  document.getElementById("days").innerHTML = days + "hari";
+  document.getElementById("hours").innerHTML = hours + "jam ";
+  document.getElementById("minutes").innerHTML = minutes + "menit ";
+  document.getElementById("seconds").innerHTML = seconds + "detik ";
+    
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
