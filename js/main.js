e064906ed20cdeecc8ee70803eb445b7c8dcd09c
riyadsarah <reysarah901@gmail.com>
2023-02-04 08:23:50 +0700

    djfafkra

--- js/main.js
+++ js/main.js
@@ -70,42 +70,12 @@ window.addEventListener("load", function(){
 
 
         //
-            var headerNav = document.getElementById("header-nav");
-
-        window.onscroll = function() {
-          if (window.pageYOffset > 100) {
-            headerNav.style.backgroundColor = "transparent";
-          } else {
-            headerNav.style.backgroundColor = "rgba(0,0,0, 0.5)";
-          }
-        };
-    
+          
         
 
 
 
 
-        function updateTime() {
-          var date = new Date();
-          var time = date.toLocaleTimeString();
-          var formattedDate = date.toLocaleDateString();
-          document.getElementById("time").innerHTML = time;
-          document.getElementById("date").innerHTML = formattedDate;
-        }
-        
-        function getWeather() {
-          // Code to fetch weather data from API goes here
-          var location = "Salamanggu, Maja";
-          var temperature = "17°C";
-          var condition = "Mountdunk Cloudy";
-          document.getElementById("location").innerHTML = location;
-          document.getElementById("temperature").innerHTML = temperature;
-          document.getElementById("condition").innerHTML = condition;
-        }
-        
-        updateTime();
-        setInterval(updateTime, 1000);
-        getWeather();
         
 
 
@@ -113,39 +83,6 @@ window.addEventListener("load", function(){
 
 
         
-
-        // Select the button
-
-     // Set the date we're counting down to
-var countDownDate = new Date("march 22, 2023 23:59:59").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Display the result in the element with id="timer"
-  document.getElementById("days").innerHTML = days + "hari";
-  document.getElementById("hours").innerHTML = hours + "jam ";
-  document.getElementById("minutes").innerHTML = minutes + "menit ";
-  document.getElementById("seconds").innerHTML = seconds + "detik ";
-    
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "EXPIRED";
-  }
-}, 1000);
 
 
 
@@ -177,41 +114,7 @@ modal.style.display = "none";
 
 //calculator
 
-          //kalkulator umur
-          function hitungUmur() {
-            var tanggalLahir = parseInt(document.getElementById("tanggal-lahir").value);
-            var bulanLahir = parseInt(document.getElementById("bulan-lahir").value);
-            var tahunLahir = parseInt(document.getElementById("tahun-lahir").value);
-            var sekarang = new Date();
-            var tanggalSekarang = sekarang.getDate();
-            var bulanSekarang = sekarang.getMonth() + 1;
-            var tahunSekarang = sekarang.getFullYear();
-            var umurTahun = tahunSekarang - tahunLahir;
-            var umurBulan = bulanSekarang - bulanLahir;
-            var umurTanggal = tanggalSekarang - tanggalLahir;
-            if (umurBulan < 0) {
-              umurTahun--;
-              umurBulan = 12 + umurBulan;
-            }
-            if (umurTanggal < 0) {
-              umurBulan--;
-              umurTanggal = 30 + umurTanggal;
-            }
-            document.getElementById("output").innerHTML = "Umur Anda adalah " + umurTahun + " tahun " + umurBulan + " bulan " + umurTanggal + " hari.";
-          }
-      
-          function resetRiwayat() {
-            document.getElementById("tanggal-lahir").value = "";
-            document.getElementById("bulan-lahir").value = "";
-            document.getElementById("tahun-lahir").value = "";
-            document.getElementById("output").innerHTML = "";
-          }
-          document.addEventListener("keyup", function(event) {
-        if (event.code === "Enter") {
-          hitungUmur();
-        }
-      });
-
+       
 
 
 
